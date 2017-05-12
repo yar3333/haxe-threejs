b16c7d4798969c80c8df64c9d6aaa8f8d656b731
Yaroslav Sivakov <yar3333@gmail.com>
2017-05-12 12:36:08 +0300

    * callback: return void

--- library/js/three/CTMLoader.hx
+++ library/js/three/CTMLoader.hx
@@ -9,13 +9,13 @@ extern class CTMLoader extends Loader
 	/**
 	 * load multiple CTM parts defined in JSON.
 	 */
-	function loadParts(url:String, callback:Void->haxe.extern.EitherType<Dynamic, Void>, parameters:Dynamic) : Dynamic;
+	function loadParts(url:String, callback:Void->Dynamic, parameters:Dynamic) : Dynamic;
 	/**
 	 * Load CTMLoader compressed models
 	 */
-	function load(url:String, callback:Dynamic->haxe.extern.EitherType<Dynamic, Void>, parameters:Dynamic) : Dynamic;
+	function load(url:String, callback:Dynamic->Dynamic, parameters:Dynamic) : Dynamic;
 	/**
 	 * create buffergeometry by ctm file.
 	 */
-	function createModel(file:String, callback:Void->haxe.extern.EitherType<Dynamic, Void>) : Dynamic;
+	function createModel(file:String, callback:Void->Dynamic) : Dynamic;
 }
