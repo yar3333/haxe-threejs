4e2125b798a5c6df4e258cd5942a49908a33608e
Yaroslav Sivakov <yar3333@gmail.com>
2024-02-10 16:26:11 +0300

    + example: base-ts

---
+++ examples/base-ts/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+	mode: 'development',
+    entry: './src/main.ts',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        alias: {
+            three: path.resolve('./node_modules/three')
+        },
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    }
+};
